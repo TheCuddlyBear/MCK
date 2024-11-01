e1cadf6c4bd203738b256de47a4723e3bb0ee4cb
TheCuddlyBear <basvthazeveld@outlook.com>
2024-11-01 23:03:18 +0100

    love

--- MCK/ui/Windows/MainWindow.cs
+++ MCK/ui/Windows/MainWindow.cs
@@ -1,11 +1,13 @@
 using Adw;
+using CmlLib.Core;
+using CmlLib.Core.ProcessBuilder;
 
 namespace MCK.ui.Windows;
 
 public class MainWindow : ApplicationWindow
 {
     private WindowTitle _windowTitle;
-    private ToolbarView _mainToolbarView;
+    private NavigationSplitView _mainToolbarView;
     private StatusPage _noProjectOpenedPage;
     private ToastOverlay _toastOverlay;
     
@@ -13,6 +15,13 @@ public class MainWindow : ApplicationWindow
     {
         Application = application;
         BuildUi();
+    }
+
+    private async void LaunchMC()
+    {
+        var launcher = new MinecraftLauncher();
+        var process = await launcher.InstallAndBuildProcessAsync("1.21", new MLaunchOption());
+        process.Start();
     }
 
     private void BuildUi()
@@ -25,15 +34,30 @@ public class MainWindow : ApplicationWindow
         SetContent(_mainToolbarView);
         return;
 
-        ToolbarView GetToolbarView()
+        NavigationSplitView GetToolbarView()
         {
             var toolbarView = new ToolbarView();
             toolbarView.AddTopBar(GetHeaderBar());
 
-            var contentBox = new Gtk.Box();
-            contentBox.Homogeneous = true;
+            var launchButton = new Gtk.Button();
+            launchButton.SetLabel("Launch MC");
+            launchButton.OnClicked += (_, __) => LaunchMC();
             
-            return toolbarView;
+            var sideBarview = new ToolbarView();
+            sideBarview.AddTopBar(GetSideHeaderBar());
+            sideBarview.SetContent(launchButton);
+
+            var sidePane = new NavigationPage();
+            sidePane.SetChild(sideBarview);
+            
+            var mainPane = new NavigationPage();
+            mainPane.SetChild(toolbarView);
+            
+            var splitView = new NavigationSplitView();
+            splitView.SetSidebar(sidePane);
+            splitView.SetContent(mainPane);
+            
+            return splitView;
         }
 
         HeaderBar GetHeaderBar()
@@ -42,8 +66,24 @@ public class MainWindow : ApplicationWindow
             var titleBox = new Gtk.Box();
             titleBox.Append(_windowTitle = new WindowTitle()
             {
+                Title = "Vanilla",
+            });
+            headerBar.SetTitleWidget(titleBox);
+            
+            /*var aboutButton = new Gtk.Button() { IconName = "help-about", TooltipText = "About program" };
+            aboutButton.OnClicked += (_ ,_) => new MCK.ui.Elements.AboutDialog().Show(this);
+            headerBar.PackEnd(aboutButton);*/
+            
+            return headerBar;
+        }
+        
+        HeaderBar GetSideHeaderBar()
+        {
+            var headerBar = new HeaderBar();
+            var titleBox = new Gtk.Box();
+            titleBox.Append(_windowTitle = new WindowTitle()
+            {
                 Title = "MCK Test",
-                Subtitle = "MCK Test"
             });
             headerBar.SetTitleWidget(titleBox);
             
